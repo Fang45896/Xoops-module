738d19b63abf8e75513d80b7adb15bc975d899d5
Shub <sa9212900@gmail.com>
2018-09-02 11:51:58 +0800

    0.1

---
+++ prodmanage/include/onInstall.php
@@ -0,0 +1,23 @@
+<?php
+include_once XOOPS_ROOT_PATH . "/modules/ugm_tools2/InstallFunction.php";
+function xoops_module_install_模組目錄(&$module) {
+  #安裝
+  go_install();
+  #更新
+  go_update();
+  return true;
+}
+
+//安裝
+function go_install() {
+	global $xoopsDB;
+	#建立資料夾	
+  mk_dir(XOOPS_ROOT_PATH . "/uploads/模組目錄");
+	return true;
+}
+
+//更新
+function go_update() {
+  global $xoopsDB;
+}
+ 
---
+++ prodmanage/include/onUninstall.php
@@ -0,0 +1,8 @@
+<?php
+function xoops_module_uninstall_模組目錄(&$module) {
+  GLOBAL $xoopsDB;
+	$date=date("Ymd");
+ 	rename(XOOPS_ROOT_PATH."/uploads/模組目錄",XOOPS_ROOT_PATH."/uploads/模組目錄_bak_{$date}");
+	return true;
+}
+
